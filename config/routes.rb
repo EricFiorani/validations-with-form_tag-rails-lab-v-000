fec73bdfd01517c93572a808cb9630c18b1895e7
Eric Fiorani <efiorani305@gmail.com>
2018-11-13 18:17:39 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "/authors/new", to: "authors#new", as: "new_author"
   get "/authors/:id", to: "authors#show", as: "author"
+  post "/authors", to: "authors#create"
+
+  get "/posts/:id", to: "posts#show", as: "post"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
